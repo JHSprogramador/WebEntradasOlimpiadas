0b208bbd236cda4b26f75b9b1af9b97aea72f4c7
byronnDev <mikel.e.g.2004@gmail.com>
2024-03-25 11:50:52 +0100

    Agregadas dependencias de autenticación de Auth0 en el archivo package.json y package-lock.json

--- FrontEnd/WebProyectoCopiaEntradas/src/app/app.component.ts
+++ FrontEnd/WebProyectoCopiaEntradas/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AuthButtonComponent } from './auth-button/auth-button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, AuthButtonComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
