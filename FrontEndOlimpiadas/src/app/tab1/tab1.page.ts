5ae07d7870b17c1b483d6a6b9a542918e843bb3f
Murillo-EL-Navajas <xabiermuro1612@gmail.com>
2024-03-26 13:15:12 +0100

    tab 1 commit :D

--- FrontEndOlimpiadas/src/app/tab1/tab1.page.ts
+++ FrontEndOlimpiadas/src/app/tab1/tab1.page.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonNav } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Tab3Page } from '../tab3/tab3.page';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonNav]
 })
 export class Tab1Page {
   constructor() {}
+  component = Tab3Page;
 }
