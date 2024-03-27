c945ef66f9348d8ec3f925c39a211e815960c842
Murillo-EL-Navajas <xabiermuro1612@gmail.com>
2024-03-27 12:20:37 +0100

    login message

--- FrontEndOlimpiadas/src/app/tab1/tab1.page.ts
+++ FrontEndOlimpiadas/src/app/tab1/tab1.page.ts
@@ -3,6 +3,7 @@ import { IonHeader, IonToolbar, IonTitle, IonContent, IonNav, IonButton, IonRow,
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { Tab3Page } from '../tab3/tab3.page';
 import { AuthButtonComponent } from '../auth-button/auth-button.component';
+import { AuthService } from '@auth0/auth0-angular';
 
 
 
@@ -14,6 +15,6 @@ import { AuthButtonComponent } from '../auth-button/auth-button.component';
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonNav, IonButton, AuthButtonComponent, IonRow, IonCol]
 })
 export class Tab1Page {
-  constructor() {}
+  constructor(public auth:AuthService) {}
   component = Tab3Page;
 }
