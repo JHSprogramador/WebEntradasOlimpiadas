b2e8345717bdfc9b9d9364add6551dfea153f571
byronnDev <mikel.e.g.2004@gmail.com>
2024-03-27 12:41:14 +0100

    Actualizar componentes de autenticación y página principal

--- FrontEndOlimpiadas/src/app/tab1/tab1.page.ts
+++ FrontEndOlimpiadas/src/app/tab1/tab1.page.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonNav, IonButton, IonRow, IonCol } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { Tab3Page } from '../tab3/tab3.page';
 import { AuthButtonComponent } from '../auth-button/auth-button.component';
 import { AuthService } from '@auth0/auth0-angular';
+import { DOCUMENT } from '@angular/common';
 
 
 
@@ -15,6 +16,6 @@ import { AuthService } from '@auth0/auth0-angular';
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonNav, IonButton, AuthButtonComponent, IonRow, IonCol]
 })
 export class Tab1Page {
-  constructor(public auth:AuthService) {}
+  constructor() {}
   component = Tab3Page;
 }
