4bc98551c581ec36b21d8da14354bf69dcbba6c0
Murillo-EL-Navajas <xabiermuro1612@gmail.com>
2024-03-27 12:00:51 +0100

    config

---
+++ FrontEndOlimpiadas/sass.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    inculdePaths:[
+        "node_modules/ionic-angular/themes",
+        "node_modules/ionicons/scss",
+        "node_modules/ionic-angular/fonts",
+        "node_modules/bootstrap/scss",
+        
+    ]
+}
