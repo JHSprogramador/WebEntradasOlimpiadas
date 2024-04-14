2e4979045c3ba87361955666c7d15d5658fca82f
byronnDev <mikel.e.g.2004@gmail.com>
2024-04-14 21:36:00 +0200

    Agregado componente de información de perfil en la página Tab 3

--- FrontEndOlimpiadas/src/app/tab3/tab3.page.ts
+++ FrontEndOlimpiadas/src/app/tab3/tab3.page.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, Inject } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { AuthService } from '@auth0/auth0-angular';
+import { CommonModule, DOCUMENT } from '@angular/common';
+import { ProfileInfoComponent } from '../profile-info/profile-info.component';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonButton, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, CommonModule, ProfileInfoComponent],
 })
 export class Tab3Page {
+  // isAuthenticated$ = this.auth.isAuthenticated$;
+  // constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) {}
   constructor() {}
 }
