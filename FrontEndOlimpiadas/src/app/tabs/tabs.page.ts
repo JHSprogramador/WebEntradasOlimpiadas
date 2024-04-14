66e231ff2ca526a445ba78edf574b271c9ebb4a1
byronnDev <mikel.e.g.2004@gmail.com>
2024-04-14 21:44:51 +0200

    Eliminar pestaña Tab 2 y actualizar iconos

--- FrontEndOlimpiadas/src/app/tabs/tabs.page.ts
+++ FrontEndOlimpiadas/src/app/tabs/tabs.page.ts
@@ -1,7 +1,7 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { home, ellipse, person } from 'ionicons/icons';
+import { home, person } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
@@ -14,6 +14,6 @@ export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ home, ellipse, person });
+    addIcons({ home, person });
   }
 }
