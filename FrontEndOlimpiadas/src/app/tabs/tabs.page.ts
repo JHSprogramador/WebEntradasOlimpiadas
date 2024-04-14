c57d1323fd5d7474cc052138dede8f9604355c6f
byronnDev <mikel.e.g.2004@gmail.com>
2024-04-14 22:00:25 +0200

    Agregada lógica para mostrar el botón de perfil solo si el usuario está autenticado

--- FrontEndOlimpiadas/src/app/tabs/tabs.page.ts
+++ FrontEndOlimpiadas/src/app/tabs/tabs.page.ts
@@ -1,19 +1,22 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { home, person } from 'ionicons/icons';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
   standalone: true,
-  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
+  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, CommonModule],
 })
 export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
+  isAuthenticated$ = this.auth.isAuthenticated$;
 
-  constructor() {
+  constructor(public auth: AuthService) {
     addIcons({ home, person });
   }
 }
