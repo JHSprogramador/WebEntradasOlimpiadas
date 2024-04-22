2fb35d46a040423ab9f8a1c385f3bd2954e9001b
Murillo-EL-Navajas <xabiermuro1612@gmail.com>
2024-04-22 13:04:38 +0200

    crear cards de eventos segun si ha pasado la fecha del sorteo

--- FrontEndOlimpiadas/src/app/auth-button/auth-button.component.ts
+++ FrontEndOlimpiadas/src/app/auth-button/auth-button.component.ts
@@ -2,20 +2,36 @@ import { AuthService } from '@auth0/auth0-angular';
 import { Component, Inject, OnInit } from '@angular/core';
 import { CommonModule, DOCUMENT } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { EventsComponent } from '../events/events.component';
 
+const mydate: string = '2024/03/31';
 @Component({
   selector: 'app-auth-button',
   templateUrl: './auth-button.component.html',
   styleUrls: ['./auth-button.component.scss'],
   standalone: true,
-  imports:[CommonModule, IonicModule]
+  imports: [CommonModule, IonicModule, EventsComponent],
 })
-export class AuthButtonComponent  implements OnInit {
-  isAuthenticated$ = this.auth.isAuthenticated$
+export class AuthButtonComponent implements OnInit {
 
-  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) { }
+  
 
+  isAuthenticated$ = this.auth.isAuthenticated$;
+  
+  constructor(
+    @Inject(DOCUMENT) public document: Document,
+    public auth: AuthService
+  ) {}
+  
   // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
   ngOnInit() {}
-
+  
+  currentDate: Date = new Date();
+  fechaLimite: Date = new Date(2024, 2, 30);
+  dateCompare() {
+    if (this.currentDate >= this.fechaLimite) {
+      return true;
+    }
+    return false;
+  }
 }
