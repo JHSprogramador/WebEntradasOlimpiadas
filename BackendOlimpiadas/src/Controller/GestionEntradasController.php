198ee77a6e5e8ccd0213c7cf2e6aff4e3ec11192
aitor <aitor@DESKTOP-GIR2RBG>
2024-04-23 14:25:45 +0200

    Comentarios para realizacion de metodos

--- BackendOlimpiadas/src/Controller/GestionEntradasController.php
+++ BackendOlimpiadas/src/Controller/GestionEntradasController.php
@@ -8,6 +8,8 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class GestionEntradasController extends AbstractController
 {
+    //GET ENTRA URL(SIN PARAMETRO)   Deportes en periodo correcto    Sale JSON
+    //RUTA: /api/olimpiadas/evento
     #[Route('/gestion/entradas', name: 'app_gestion_entradas')]
     public function index(): Response
     {
@@ -15,4 +17,18 @@ class GestionEntradasController extends AbstractController
             'controller_name' => 'GestionEntradasController',
         ]);
     }
+
+    //GET ENTRA URL(CON PARAMETRO ID DEPORTE)    de eventos por id deporte    Sale JSON
+    //RUTA: /api/olimpiadas/deporte
+
+    //GET ENTRA URL(CON PARAMETRO ID USUARIO) Get_ de todö (Evento, deporte, seccion, estadio)
+    //segun una id solo si la semana es la del usuario y
+    // solo se regresan las del periodo correcto
+    //RUTA: /api/olimpiadas/actividades
+
+    //Metodo Comprobar el periodo actual
+
+    //Metodo Recivo una id de usuario, compruebo si la semana actual es correcta para el usuario
+
+
 }
