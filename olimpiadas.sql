eaaedae7f02284f9baed1ddd04aa95d60dfdbf5b
Juan Hidalgo <88659441+JHSprogramador@users.noreply.github.com>
2024-04-23 13:55:52 +0200

    nueva base de datos

--- olimpiadas.sql
+++ olimpiadas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-04-2024 a las 23:28:28
+-- Tiempo de generación: 23-04-2024 a las 13:53:29
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -24,6 +24,17 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `deportes`
+--
+
+CREATE TABLE `deportes` (
+  `id` int(11) NOT NULL,
+  `nombre_deporte` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `doctrine_migration_versions`
 --
 
@@ -38,7 +49,55 @@ CREATE TABLE `doctrine_migration_versions` (
 --
 
 INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
-('DoctrineMigrations\\Version20240414212717', '2024-04-14 23:27:23', 81);
+('DoctrineMigrations\\Version20240423085514', '2024-04-23 10:55:25', 126),
+('DoctrineMigrations\\Version20240423110738', '2024-04-23 13:08:34', 429);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `entrada`
+--
+
+CREATE TABLE `entrada` (
+  `id` int(11) NOT NULL,
+  `id_usuario_id` int(11) DEFAULT NULL,
+  `id_seccion_evento_id` int(11) DEFAULT NULL,
+  `id_transaccion_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estadios`
+--
+
+CREATE TABLE `estadios` (
+  `id` int(11) NOT NULL,
+  `nombre_estadio` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `eventos`
+--
+
+CREATE TABLE `eventos` (
+  `id` int(11) NOT NULL,
+  `nombre_evento` varchar(255) NOT NULL,
+  `periodo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `eventos_deportes`
+--
+
+CREATE TABLE `eventos_deportes` (
+  `eventos_id` int(11) NOT NULL,
+  `deportes_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -59,6 +118,43 @@ CREATE TABLE `messenger_messages` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `secciones`
+--
+
+CREATE TABLE `secciones` (
+  `id` int(11) NOT NULL,
+  `id_estadio_id` int(11) DEFAULT NULL,
+  `aforo` int(11) NOT NULL,
+  `nombre_seccion` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `seccion_evento`
+--
+
+CREATE TABLE `seccion_evento` (
+  `id` int(11) NOT NULL,
+  `id_seccion_id` int(11) DEFAULT NULL,
+  `id_evento_id` int(11) DEFAULT NULL,
+  `precio` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `transaxxiones`
+--
+
+CREATE TABLE `transaxxiones` (
+  `id` int(11) NOT NULL,
+  `fecha_transaccion` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuario`
 --
 
@@ -66,8 +162,7 @@ CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
   `id_auth0` varchar(255) NOT NULL,
   `mail` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `foto` varchar(255) NOT NULL
+  `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -88,10 +183,45 @@ CREATE TABLE `usuarios_meses` (
 --
 
 --
+-- Indices de la tabla `deportes`
+--
+ALTER TABLE `deportes`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `doctrine_migration_versions`
 --
 ALTER TABLE `doctrine_migration_versions`
   ADD PRIMARY KEY (`version`);
+
+--
+-- Indices de la tabla `entrada`
+--
+ALTER TABLE `entrada`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_C949A2747EB2C349` (`id_usuario_id`),
+  ADD KEY `IDX_C949A27417A0767C` (`id_seccion_evento_id`),
+  ADD KEY `IDX_C949A274B0DFD449` (`id_transaccion_id`);
+
+--
+-- Indices de la tabla `estadios`
+--
+ALTER TABLE `estadios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `eventos`
+--
+ALTER TABLE `eventos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `eventos_deportes`
+--
+ALTER TABLE `eventos_deportes`
+  ADD PRIMARY KEY (`eventos_id`,`deportes_id`),
+  ADD KEY `IDX_FDBC7DD87F243861` (`eventos_id`),
+  ADD KEY `IDX_FDBC7DD81F308F18` (`deportes_id`);
 
 --
 -- Indices de la tabla `messenger_messages`
@@ -101,6 +231,27 @@ ALTER TABLE `messenger_messages`
   ADD KEY `IDX_75EA56E0FB7336F0` (`queue_name`),
   ADD KEY `IDX_75EA56E0E3BD61CE` (`available_at`),
   ADD KEY `IDX_75EA56E016BA31DB` (`delivered_at`);
+
+--
+-- Indices de la tabla `secciones`
+--
+ALTER TABLE `secciones`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_E7BAC5CA4039A3B9` (`id_estadio_id`);
+
+--
+-- Indices de la tabla `seccion_evento`
+--
+ALTER TABLE `seccion_evento`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_8A03286BDFD0C1ED` (`id_seccion_id`),
+  ADD KEY `IDX_8A03286B7904465` (`id_evento_id`);
+
+--
+-- Indices de la tabla `transaxxiones`
+--
+ALTER TABLE `transaxxiones`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `usuario`
@@ -120,10 +271,52 @@ ALTER TABLE `usuarios_meses`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `deportes`
+--
+ALTER TABLE `deportes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `entrada`
+--
+ALTER TABLE `entrada`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `estadios`
+--
+ALTER TABLE `estadios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `eventos`
+--
+ALTER TABLE `eventos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `messenger_messages`
 --
 ALTER TABLE `messenger_messages`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `secciones`
+--
+ALTER TABLE `secciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `seccion_evento`
+--
+ALTER TABLE `seccion_evento`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `transaxxiones`
+--
+ALTER TABLE `transaxxiones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
@@ -140,6 +333,34 @@ ALTER TABLE `usuarios_meses`
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `entrada`
+--
+ALTER TABLE `entrada`
+  ADD CONSTRAINT `FK_C949A27417A0767C` FOREIGN KEY (`id_seccion_evento_id`) REFERENCES `seccion_evento` (`id`),
+  ADD CONSTRAINT `FK_C949A2747EB2C349` FOREIGN KEY (`id_usuario_id`) REFERENCES `usuario` (`id`),
+  ADD CONSTRAINT `FK_C949A274B0DFD449` FOREIGN KEY (`id_transaccion_id`) REFERENCES `transaxxiones` (`id`);
+
+--
+-- Filtros para la tabla `eventos_deportes`
+--
+ALTER TABLE `eventos_deportes`
+  ADD CONSTRAINT `FK_FDBC7DD81F308F18` FOREIGN KEY (`deportes_id`) REFERENCES `deportes` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_FDBC7DD87F243861` FOREIGN KEY (`eventos_id`) REFERENCES `eventos` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `secciones`
+--
+ALTER TABLE `secciones`
+  ADD CONSTRAINT `FK_E7BAC5CA4039A3B9` FOREIGN KEY (`id_estadio_id`) REFERENCES `estadios` (`id`);
+
+--
+-- Filtros para la tabla `seccion_evento`
+--
+ALTER TABLE `seccion_evento`
+  ADD CONSTRAINT `FK_8A03286B7904465` FOREIGN KEY (`id_evento_id`) REFERENCES `eventos` (`id`),
+  ADD CONSTRAINT `FK_8A03286BDFD0C1ED` FOREIGN KEY (`id_seccion_id`) REFERENCES `secciones` (`id`);
 
 --
 -- Filtros para la tabla `usuarios_meses`
