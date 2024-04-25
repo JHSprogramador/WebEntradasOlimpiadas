113b77bd4c1811b594ea726a61255e5b4860d949
byronnDev <mikel.e.g.2004@gmail.com>
2024-04-25 09:27:01 +0200

    Actualizaciones de configuración de CORS, controlador de gestión de usuarios y componente AuthButton

--- FrontEndOlimpiadas/src/app/service.service.ts
+++ FrontEndOlimpiadas/src/app/service.service.ts
@@ -2,25 +2,16 @@ import { AuthService } from '@auth0/auth0-angular';
 // import jquery from assets/jquery.min.js
 import * as $ from 'jquery';
 import { firstValueFrom, map } from 'rxjs';
-// User
-export interface User {
-  id: number;
-  auth0Id: string;
-  sub: string;
-  name: string;
-  mail: string;
-}
 const apiURL = 'http://localhost:8000/api';
 
 export class ServiceService {
-  auth0User = this.auth.user$;
   constructor(public auth: AuthService) {
   }
 
   async pushUser() {
-    const idAuth0 = await firstValueFrom(this.auth0User.pipe(map(user => user?.sub)));
-    const name = await firstValueFrom(this.auth0User.pipe(map(user => user?.name)));
-    const mail = await firstValueFrom(this.auth0User.pipe(map(user => user?.email)));
+    const idAuth0 = await firstValueFrom(this.auth.user$.pipe(map(user => user?.sub)));
+    const name = await firstValueFrom(this.auth.user$.pipe(map(user => user?.name)));
+    const mail = await firstValueFrom(this.auth.user$.pipe(map(user => user?.email)));
 
     const body = JSON.stringify({
       idAuth0,
