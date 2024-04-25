4773b8b6bf6dc0199b51f8682bdbfdabd757d6e1
JHSprogramador <hsjuan7@gmail.com>
2024-04-25 13:45:52 +0200

    recibimo id de deporte por url devovemos todos los eventos de ese deporte por json

--- BackendOlimpiadas/src/Controller/GestionEntradasController.php
+++ BackendOlimpiadas/src/Controller/GestionEntradasController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Eventos;
 use App\Entity\UsuariosMeses;
 use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,30 +16,26 @@ class GestionEntradasController extends AbstractController
 {
     //GET ENTRA URL(SIN PARAMETRO)   Deportes en periodo correcto    Sale JSON
     //RUTA: /api/olimpiadas/evento
-    #[Route('/evento', name: 'get_eventos', methods: ['GET'])]
-    public function getEvento(EntityManagerInterface $entityManager): JsonResponse
+    
+    #[Route('/evento/{id}', name: 'get_eventos', methods: ['GET'])]
+    public function getEventos(EntityManagerInterface $entityManager): JsonResponse
     {
-        $periodo = $this->ComprobarPeriodo();
-        if ($periodo == 0) {
-            return new JsonResponse(['error' => 'No hay eventos en este periodo'], Response::HTTP_NOT_FOUND);
-        }
-        $query = $entityManager->createQuery('SELECT e FROM App\Entity\Eventos e WHERE e.periodo = :periodo'); // la e es de evento (se usa e para abreviar el nombre de la tabla)
-        $query->setParameter('periodo', $periodo);
-        $eventos = $query->getResult();
+        //recibimo id de deporte por url un valor (1 o 2)
+        $id_deporte = $_GET['id_deporte'];
+        // devovemos todos los eventos de ese deporte por json
+        $eventos = $entityManager->getRepository(Eventos::class)->findBy(['id_deporte' => $id_deporte]);
         $data = [];
+
         foreach ($eventos as $evento) {
             $data[] = [
-                'id' => $evento->getId(),
-                'nombre' => $evento->getNombre(),
-                'fecha' => $evento->getFecha(),
-                'hora' => $evento->getHora(),
-                'periodo' => $evento->getPeriodo(),
-                'deporte' => $evento->getDeporte()->getNombre(),
-                'seccion' => $evento->getSeccion()->getNombre(),
-                'estadio' => $evento->getEstadio()->getNombre()
+            'id' => $evento->getId(),
+            'nombre' => $evento->getNombre(),
             ];
         }
-        return new JsonResponse($data, Response::HTTP_OK);
+
+        return new JsonResponse($data, 200);
+        
+
     }
 
     //GET ENTRA URL(CON PARAMETRO ID DEPORTE)    de eventos por id deporte    Sale JSON
@@ -46,8 +43,8 @@ class GestionEntradasController extends AbstractController
     #[Route('/deporte', name: 'get_deportes', methods: ['GET'])]
     public function getDeportes(EntityManagerInterface $entityManager): JsonResponse
     {
-        // TODO: Implementar este método
-        return new JsonResponse(['error' => 'No hay eventos en este periodo'], Response::HTTP_NOT_FOUND);
+
+        
     }
     //GET ENTRA URL(CON PARAMETRO ID AUTH0) Get_ de todo (Evento, deporte, seccion, estadio)
     //segun una id solo si la semana es la del usuario y
@@ -56,8 +53,10 @@ class GestionEntradasController extends AbstractController
     #[Route('/actividades', name: 'get_actividades', methods: ['GET'])]
     public function getActividades(EntityManagerInterface $entityManager): JsonResponse
     {
-        // TODO: Implementar este método
-        return new JsonResponse(['error' => 'No hay eventos en este periodo'], Response::HTTP_NOT_FOUND);
+
+        
+
+
     }
     //Metodo Comprobar el periodo actual
     public function ComprobarPeriodo(): int
@@ -77,7 +76,7 @@ class GestionEntradasController extends AbstractController
     //Metodo Recivo una id de usuario, compruebo si la semana actual es correcta para el usuario
     public function ComprobarSemana(UsuariosMeses $usuario): bool
     {
-
+        //TODO: Comprobar si la semana actual es la correcta para el usuario
 
         return false;
     }
