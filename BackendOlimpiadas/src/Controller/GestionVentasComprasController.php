17e9c04ba93118003ec875ca02789fb7b78deee3
aitor <aitor@DESKTOP-GIR2RBG>
2024-04-26 08:45:16 +0200

    Creacion metodo ComprobarCantidad de ventas

--- BackendOlimpiadas/src/Controller/GestionVentasComprasController.php
+++ BackendOlimpiadas/src/Controller/GestionVentasComprasController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Usuario;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -21,5 +23,17 @@ class GestionVentasComprasController extends AbstractController
         ]);
     }
 
-    
+    //Parametros: 
+    //  Usuario a comprobar
+    //  Cantidad que ha comprado
+    //  La id de seccionEvento que va a comprar
+    //Salida:
+    //  booleano que indica si puede o no realizar la compra
+    //Descripcion: el usuario no puede comprar mas de 5 entradas se un mismo deporte
+    public function comprobarCantidadComprada(Usuario $usuario, ObjectManager $manager, int $cantidad, int $idDeporte): bool{
+        
+        
+
+        return false;
+    }
 }
